6c981be7a99469b6d9c86bc917f1e96b105b30f2
MelihCK <kanmaz_can@hotmail.com>
2022-10-29 17:00:08 +0300

    after not implemented error

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,10 +7,11 @@ require("hardhat-contract-sizer")
 require("dotenv").config()
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli"
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "*"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "*"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "*"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "*"
+const REPORT_GAS = process.env.REPORT_GAS || false
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -27,7 +28,8 @@ module.exports = {
             chainId: 5,
             blockConfirmations: 6,
             url: GOERLI_RPC_URL,
-            accounts: [PRIVATE_KEY],
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            saveDeployments: true,
         },
     },
     etherscan: {
@@ -37,17 +39,27 @@ module.exports = {
         },
     },
     gasReporter: {
-        enabled: false,
+        enabled: REPORT_GAS,
         outputFile: "gas-report.txt",
         noColors: true,
         currency: "USD",
         coinmarketcap: COINMARKETCAP_API_KEY,
         token: "ETH",
     },
-    solidity: "0.8.7",
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.7",
+            },
+            {
+                version: "0.4.24",
+            },
+        ],
+    },
     namedAccounts: {
         deployer: {
             default: 0,
+            1: 0,
         },
         player: {
             default: 1,
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,6 +1,10 @@
 const { ethers } = require("hardhat")
 
 const networkConfig = {
+    default: {
+        name: "hardhat",
+        keepersUpdateInterval: "30",
+    },
     5: {
         name: "goerli",
         vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
@@ -11,7 +15,7 @@ const networkConfig = {
         interval: "30",
     },
     31337: {
-        name: "hardhat",
+        name: "localhost",
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
         callbackGasLimit: "500000",
@@ -20,8 +24,14 @@ const networkConfig = {
 }
 
 const developmentChains = ["hardhat", "localhost"]
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6
+const frontEndContractsFile = "../nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json"
+const frontEndAbiFile = "../nextjs-smartcontract-lottery-fcc/constants/abi.json"
 
 module.exports = {
     networkConfig,
     developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+    frontEndContractsFile,
+    frontEndAbiFile,
 }
